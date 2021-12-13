ce2c4afe84f01c2c949530ed1f9b731c583d1420
Mitchell <mlabenski123@gmail.com>
2021-12-12 21:36:23 -0500

    test

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify'
+/* Import the firebase SDK and extend with firestore */
+import firebase from 'firebase';
+require('firebase/firestore');
 
 Vue.config.productionTip = false
+firebase.initializeApp(config);
+Vue.prototype.$firebase = firebase;
 
 new Vue({
   vuetify,

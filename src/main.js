fa26b433072035b2bb92e375a094db1cf2dac4f7
Mitchell <mlabenski123@gmail.com>
2021-12-12 21:37:00 -0500

    test

--- src/main.js
+++ src/main.js
@@ -7,6 +7,14 @@ import firebase from 'firebase';
 require('firebase/firestore');
 
 Vue.config.productionTip = false
+const config = {
+  apiKey: "AIzaSyAdBUWBWab68z8Eo0ldaaFzfD9LEvGtkcQ",
+  authDomain: "insider-stall.firebaseapp.com",
+  projectId: "insider-stall",
+  storageBucket: "insider-stall.appspot.com",
+  messagingSenderId: "426981076390",
+  appId: "1:426981076390:web:90f6b290ff6d571e917367"
+};
 firebase.initializeApp(config);
 Vue.prototype.$firebase = firebase;
 

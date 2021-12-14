6253df3b6188e8a6dffafb09de192bab7f32b6fc
Mitchell <mlabenski123@gmail.com>
2021-12-13 22:46:39 -0500

    connecting to fb data

--- src/App.vue
+++ src/App.vue
@@ -1,113 +1,221 @@
 <template>
-  <v-card :class="colorOfCard"  class="mx-auto my-12 darken-2 white--text" max-width="600" id="floor-card">
-    <v-card-title class="dark-orange">{{title}}</v-card-title>
-    <v-card-text>
-      <div class="white--text">Floor one has reports of little foot traffic, and an average usage time of 2 minutes. Expect longer wait times between 8am-9:30am and 4pm-5:30pm.</div>
-    </v-card-text>
-    <v-chip-group v-model="genderSelection" active-class="black white--text" column>
-        <v-chip style="margin-left: 25%;">Male</v-chip>
-        <v-chip >Female</v-chip>
-      </v-chip-group>
-    <v-divider class="mx-4"></v-divider>
-        <v-card-title text="center" v-if="genderSelection==0">Men's Room</v-card-title>
-        <v-card-title text="center" v-if="genderSelection==1">Female's Room</v-card-title>
-    <v-card-text>
-      <v-chip-group v-model="stallSelection" style="margin-left: 17%;" column active-class="green white--text">
-        <v-chip v-for="stall in stallData" :key="stall.id" :disabled="stall.occupied">Stall {{stall.id}}</v-chip>
-      </v-chip-group>
-    </v-card-text>
-    <v-card-title text="center">Reserve Ahead</v-card-title>
-    <v-card-text>
-      <v-chip-group v-model="selection" active-class="black white--text" column>
-        <v-chip>5:30PM</v-chip>
-        <v-chip>7:30PM</v-chip>
-        <v-chip>8:00PM</v-chip>
-      </v-chip-group>
-      <v-divider class="mx-2"></v-divider>
-      <v-row>
-                      <v-btn style="margin-top: 35px; width: 45%;"
-          elevation="2" @click="bookStall(stallSelection, genderSelection, floor_num)"
-        >Book Now</v-btn>
-                <v-btn style=" margin-top: 35px; width: 45%; margin-left: 10%;" 
-          elevation="2"
-        >Reserve</v-btn>
-      </v-row>
-    </v-card-text>
-  </v-card>
+  <v-app>
+    <v-app-bar app dark>
+      <div class="d-flex align-center">
+        <h1>Insider Stall Test123</h1>
+      </div>
+    </v-app-bar>
+    <v-main class="container">
+      <div class="fixed fixed--center" style="z-index: 3">
+              <v-menu
+      transition="slide-y-transition"
+      bottom
+    >
+      <template v-slot:activator="{ on, attrs }" style="z-index: 1; width: 90%;">
+        <v-btn
+          class="purple rounded-borders card"
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+           style="z-index: 1; height: 35px; width: 85%"
+        >
+          {{current.text}}
+        </v-btn>
+                    <v-btn
+              color="red"
+              fab
+              small
+              dark
+              style="left: 9px"
+            >
+              <v-icon>mdi-bell-ring</v-icon>
+            </v-btn>
+      </template>
+      <v-list>
+        <v-list-item 
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+        <Vue2InteractDraggable
+          v-if="isVisible"
+          :interact-x-threshold="100"
+          @draggedRight="accept"
+          @draggedLeft="decline"
+          class="rounded-borders card"
+        >
+          <FloorCard @clicked="onGenderSwitch" @booking="onBooking" :title="current.text" :stallData="dataInView"/>
+        </Vue2InteractDraggable>
+      </div>
+    <div
+      v-if="next"
+      class="rounded-borders card card--two fixed fixed--center"
+      style="z-index: 2">
+      <FloorCard :title="next.text" />
+    </div>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+import { Vue2InteractDraggable } from "vue2-interact";
+import FloorCard from '@/components/FloorCard'
+import { db } from '@/services/firebase'
+
 export default {
-    name: 'floor-card',
-    data: ()=> ({
-        selection: 1,
-        stallSelection: null,
-        genderSelection: 0,
-        isOrange: true,
-        colorOfCard: 'orange',
-    }),
-    props: {
-        floor_num: {
-            type: Number,
-            default: 0
-        },
-        title: {
-            type: String,
-            default: 'Floor Name'
-        },
-        stallData: {
-          type: Array,
-        }
+  name: "app",
+  components: { Vue2InteractDraggable, FloorCard },
+  data() {
+    return {
+      isVisible: true,
+      index: 0,
+      selection: 1,
+      genderSelected : 'b',
+      //pipe the current data into cards. like here.
+      // what data should be passed?
+      cards: [
+        { text: "floor one" },
+        { text: "floor two" },
+        { text: "floor three" },
+      ],
+      items: [
+        { title: 'Floor one' },
+        { title: 'Floor two' },
+        { title: 'Floor three' },
+        { title: 'Floor four' },
+        { title: 'Floor Five' },
+      ],
+      testFirebaseData: [],
+      dataInView: []
+    };
+  },
+  mounted() {
+    this.loadFloors()
+    //should be able to pass this out right
+  },
+  computed: {
+    current() {
+      return this.cards[this.index];
     },
-    watch: {
-      genderSelection: function() {
-        if(this.genderSelection == 0){
-          this.colorOfCard = 'orange';
-          this.$emit('clicked', 'b');
-        }
-        else if (this.genderSelection == 1){
-          this.colorOfCard = 'teal';
-          this.$emit('clicked', 'f')
-        }
-      },
-      stallSelection: function() {
-        console.log(this.stallSelection);
-      },
-      stallData: function() {
-        console.log('stall data is ---> ')
-        this.stallData.forEach(element => {
-          console.log(element.id);
-          console.log(element.occupied);
+    next() {
+      return this.cards[this.index + 1]
+    }
+  },
+  methods: {
+    loadFloors() {
+      db.collection("stall_id")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+           this.testFirebaseData.push({ ...doc.data() });
+          });
+        })
+        .catch((error) => {
+          console.log("Error getting documents: ", error);
         });
+    },
+    accept() {
+      if(this.index!=2){
+      setTimeout(() => this.isVisible = false, 200)
+      setTimeout(() => {
+        this.index++
+        this.isVisible = true
+        this.loadFloorData();
+      }, 300)
       }
     },
-    methods: {
-      bookStall(stallNum, gender, floor){
-        if(stallNum!=null && gender!=null && floor!=null){
-          console.log('stall has been booked');
-          var id = '';
-          //create the id to compare w. firebase data
-          if(gender == 0){ //gender=0 is a male
-            id = String([floor+1, 'b', stallNum+1])
-            id = id.replace(/[, ]+/g, "").trim();
-          }
-          if(gender ==1){ //gender=1 is a female
-            id = String([floor+1, 'f', stallNum+1])
-            id = id.replace(/[, ]+/g, "").trim();
-          }
-          this.$emit('booking', id)
-        }
-        else {
-          alert('some data was not added: booking not available at this time.')
-        }
+    decline() {
+      if(this.index!=0){
+      setTimeout(() => (this.isVisible = false), 200);
+      setTimeout(() => {
+        this.index--;
+        this.isVisible = true;
+        this.loadFloorData();
+      }, 300);
       }
     },
-}
+    onGenderSwitch(value) {
+      console.log('parent knows of the new value ' + value)
+      this.genderSelected = value;
+      this.loadFloorData();
+    },
+    onBooking(value){
+      console.log('A booking has been made at stall: '+value+ ' now we send to firebase');
+      db.collection("stall_id").doc(value).update({occupied: true});
+    },
+    loadFloorData(){
+      //index is == to the floor
+      //genderSelected is == to the gender choice ('m' or 'f')
+      //all data is stored in testFirebaseData
+      var floorValue = this.index+1;
+      var finalData = [];
+      console.log('the selected floor is '+ floorValue);
+      console.log('the selected gender is '+ this.genderSelected);
+      for (let i =0; i < this.testFirebaseData.length; i++){
+        //first check for floor
+        if(this.testFirebaseData[i].id.charAt(0) == floorValue && this.testFirebaseData[i].id.charAt(1) == this.genderSelected){
+            console.log('found a match '+ this.testFirebaseData[i].id);
+            finalData.push(this.testFirebaseData[i]);
+        }
+      }
+      this.dataInView = finalData;
+    }
+  },
+  watch: {
+    testFirebaseData: function() {
+      console.log('watcher was hit!: ')
+      console.log('current floor index '+ this.index)
+       this.loadFloorData();
+      //this is only going to load the available data for the current front-end configuration
+      //when a firebase collection change occurs..... unless we need to watch for changes in the fb collection to?
+     // this.loadFloorData()
+    }
+  }
+};
 </script>
 
-<style>
-.centerElem {
-  left: 30%;
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  height: 100vh;
+}
+
+.flex {
+  display: flex;
+  &--center {
+    align-items: center;
+    justify-items: center;
+    justify-content: center;
+  }
+}
+
+.fixed {
   position: fixed;
+  &--center {
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
+.rounded-borders {
+  border-radius: 12px;
+}
+.card {
+  width: 300px;
+  height: 300px;
+  color: white;
+  &--two {
+    width: 280px;
+    top: 49%;
+  }
+  &--three {
+    width: 260px;
+    top: 40%;
+  }
 }
 </style>
--- src/components/FloorCard.vue
+++ src/components/FloorCard.vue
@@ -1,18 +1,37 @@
 <template>
-  <v-card class="mx-auto my-12 orange darken-2 white--text" max-width="374" id="floor-card">
-    <v-card-title>{{ title }}</v-card-title>
+  <v-card :class="colorOfCard"  class="mx-auto my-12 darken-2 white--text" max-width="600" id="floor-card">
+    <v-card-title class="dark-orange">{{title}}</v-card-title>
     <v-card-text>
-      <div class="white--text">Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+      <div class="white--text">Floor one has reports of little foot traffic, and an average usage time of 2 minutes. Expect longer wait times between 8am-9:30am and 4pm-5:30pm.</div>
     </v-card-text>
+    <v-chip-group v-model="genderSelection" active-class="black white--text" column>
+        <v-chip style="margin-left: 25%;">Male</v-chip>
+        <v-chip >Female</v-chip>
+      </v-chip-group>
     <v-divider class="mx-4"></v-divider>
-    <v-card-title>Availability</v-card-title>
+        <v-card-title text="center" v-if="genderSelection==0">Men's Room</v-card-title>
+        <v-card-title text="center" v-if="genderSelection==1">Female's Room</v-card-title>
+    <v-card-text>
+      <v-chip-group v-model="stallSelection" style="margin-left: 17%;" column active-class="green white--text">
+        <v-chip v-for="stall in stallData" :key="stall.id" :disabled="stall.occupied">Stall {{stall.id}}</v-chip>
+      </v-chip-group>
+    </v-card-text>
+    <v-card-title text="center">Reserve Ahead</v-card-title>
     <v-card-text>
       <v-chip-group v-model="selection" active-class="black white--text" column>
         <v-chip>5:30PM</v-chip>
         <v-chip>7:30PM</v-chip>
         <v-chip>8:00PM</v-chip>
-        <v-chip>9:00PM</v-chip>
       </v-chip-group>
+      <v-divider class="mx-2"></v-divider>
+      <v-row>
+                      <v-btn style="margin-top: 35px; width: 45%;"
+          elevation="2" @click="bookStall(stallSelection, genderSelection, floor_num)"
+        >Book Now</v-btn>
+                <v-btn style=" margin-top: 35px; width: 45%; margin-left: 10%;" 
+          elevation="2"
+        >Reserve</v-btn>
+      </v-row>
     </v-card-text>
   </v-card>
 </template>
@@ -21,7 +40,11 @@
 export default {
     name: 'floor-card',
     data: ()=> ({
-        selection: 1
+        selection: 1,
+        stallSelection: null,
+        genderSelection: 0,
+        isOrange: true,
+        colorOfCard: 'orange',
     }),
     props: {
         floor_num: {
@@ -32,10 +55,59 @@ export default {
             type: String,
             default: 'Floor Name'
         },
-    }
+        stallData: {
+          type: Array,
+        }
+    },
+    watch: {
+      genderSelection: function() {
+        if(this.genderSelection == 0){
+          this.colorOfCard = 'orange';
+          this.$emit('clicked', 'b');
+        }
+        else if (this.genderSelection == 1){
+          this.colorOfCard = 'teal';
+          this.$emit('clicked', 'f')
+        }
+      },
+      stallSelection: function() {
+        console.log(this.stallSelection);
+      },
+      stallData: function() {
+        console.log('stall data is ---> ')
+        this.stallData.forEach(element => {
+          console.log(element.id);
+          console.log(element.occupied);
+        });
+      }
+    },
+    methods: {
+      bookStall(stallNum, gender, floor){
+        if(stallNum!=null && gender!=null && floor!=null){
+          console.log('stall has been booked');
+          var id = '';
+          //create the id to compare w. firebase data
+          if(gender == 0){ //gender=0 is a male
+            id = String([floor+1, 'b', stallNum+1])
+            id = id.replace(/[, ]+/g, "").trim();
+          }
+          if(gender ==1){ //gender=1 is a female
+            id = String([floor+1, 'f', stallNum+1])
+            id = id.replace(/[, ]+/g, "").trim();
+          }
+          this.$emit('booking', id)
+        }
+        else {
+          alert('some data was not added: booking not available at this time.')
+        }
+      }
+    },
 }
 </script>
 
 <style>
-
+.centerElem {
+  left: 30%;
+  position: fixed;
+}
 </style>

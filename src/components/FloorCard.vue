e50c6884178c677a491b4e341616efaf6eff5e2b
Mitchell <mlabenski123@gmail.com>
2021-12-15 23:46:36 -0500

    timer countdown setup

--- src/components/FloorCard.vue
+++ src/components/FloorCard.vue
@@ -1,4 +1,4 @@
-<template>
+<template v-if="stallData">
   <v-card :class="colorOfCard"  class="mx-auto my-12 darken-2 white--text" max-width="600" id="floor-card">
     <v-card-title class="dark-orange">{{title}}</v-card-title>
     <v-card-text>
@@ -13,7 +13,7 @@
         <v-card-title text="center" v-if="genderSelection==1">Female's Room</v-card-title>
     <v-card-text>
       <v-chip-group v-model="stallSelection" style="margin-left: 17%;" column active-class="green white--text">
-        <v-chip v-for="stall in stallData" :key="stall.id" :disabled="stall.occupied">Stall {{stall.id}}</v-chip>
+        <v-chip v-for="stall in stallData" :key="stall.id" :disabled="stall.occupied">{{stall.occupied ? 'occupied for:'+ stall.duration: stall.id}}</v-chip>
       </v-chip-group>
     </v-card-text>
     <v-card-title text="center">Reserve Ahead</v-card-title>
@@ -44,8 +44,48 @@ export default {
         stallSelection: null,
         genderSelection: 0,
         isOrange: true,
+        timerOne: null,
+        timerOneRunning: false,
+        timeOne: 0,
+        timerTwo: null,
+        timerTwoRunning: false,
+        timeTwo: 0,
+        occupiedCheckOne: false,
+        occupiedCheckTwo: false,
         colorOfCard: 'orange',
     }),
+    created() {
+      this.timerOneRunning == false;
+      clearInterval(this.timerOne);
+      this.timerTwoRunning == false;
+      clearInterval(this.timerTwo);
+      console.log('did this even run yet')
+      this.timerOne = null;
+      this.timerTwo = null;
+      this.timeOne = 0;
+      this.timeTwo = 0;
+        for(var stall in this.stallData) {
+          console.log('example stall: '+this.stallData[stall].duration);
+          // if one of the stalls are occupied
+          if(this.stallData[stall].occupied){
+            console.log('warning atleast 1 stall is occupied')
+            console.log(stall);
+            if(stall == 0){
+              //its occupied, so lets attach a timer to this stall
+              console.log('warning stall 0 is occupied- starting a timer');
+              //stall 1 is occupied, so begin the timer from the elapsed seconds + counting
+              this.toggleTimer(0);
+            }
+            if(stall == 1){
+              console.log('warning stall 0 is occupied- starting a timer');
+              this.toggleTimer(1);
+            }
+          }
+          else if(!this.stallData[stall].occupied){
+              console.log('Stall '+ this.stallData[stall].id + ' is available!')
+          }
+        }
+    },
     props: {
         floor_num: {
             type: Number,
@@ -72,7 +112,8 @@ export default {
       },
       stallSelection: function() {
         console.log(this.stallSelection);
-      },
+        this.checkForDisabled();
+      }
     },
     methods: {
       bookStall(stallNum, gender, floor){
@@ -93,6 +134,43 @@ export default {
         else {
           alert('Error: some data was not added, therefore booking not available at this time.')
         }
+      },
+      checkForDisabled() {
+        for(var stall in this.stallData) {
+          if(this.stallData[stall].occupied){
+              console.log('Stall '+ this.stallData[stall].id + ' has been occupied for: '+ this.stallData[stall].duration);
+
+          }
+          else if(!this.stallData[stall].occupied){
+              console.log('Stall '+ this.stallData[stall].id + ' has not been occupied for')
+          }
+        }
+      },
+      toggleTimer(stallIndex) {
+        console.log('timer started')
+        //var elapsedTime = this.stallData[stallIndex].duration;
+        if (this.timerOneRunning && stallIndex == 0) {
+          this.timerOneRunning == false;
+          clearInterval(this.timerOne);
+        }
+        else if (!this.timerOneRunning && stallIndex == 0){
+          this.timerOneRunning == true;
+          this.timerOne = setInterval(this.incrementTimeZero, 1000);
+        }
+        else if (this.timerTwoRunning && stallIndex == 1){
+          this.timerTwoRunning == false;
+          clearInterval(this.timerTwo);
+        }
+        else if(!this.timerTwoRunning && stallIndex == 1){
+          this.timerTwoRunning == true;
+           this.timerTwo = setInterval(this.incrementTimeOne, 1000)
+        }
+      },
+      incrementTimeZero() {
+        this.stallData[0].duration =this.stallData[0].duration+1;
+      },
+      incrementTimeOne() {
+        this.stallData[1].duration =this.stallData[1].duration+1;
       }
     },
 }

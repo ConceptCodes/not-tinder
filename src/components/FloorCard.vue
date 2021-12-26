eca2550312c7feedcdd207ed01bf6762a516bff2
Mitchell LaBenski <mlabenski123@gmail.com>
2021-12-25 23:56:42 -0500

    vuex for floor card

--- src/components/FloorCard.vue
+++ src/components/FloorCard.vue
@@ -2,7 +2,7 @@
   <v-card :class="colorOfCard"  class="mx-auto my-12 darken-2 white--text" max-width="600" id="floor-card">
     <v-card-title class="dark-orange">{{title}}</v-card-title>
     <v-card-text>
-      <div class="white--text">Floor one has reports of little foot traffic, and an average usage time of 2 minutes. Expect longer wait times between 8am-9:30am and 4pm-5:30pm.</div>
+      <div class="white--text">HELLO HELLO, and an average usage time of 2 minutes. Expect longer wait times between 8am-9:30am and 4pm-5:30pm.</div>
     </v-card-text>
     <v-chip-group v-model="genderSelection" active-class="black white--text" column>
         <v-chip style="margin-left: 25%;">Male</v-chip>
@@ -13,7 +13,7 @@
         <v-card-title text="center" v-if="genderSelection==1">Female's Room</v-card-title>
     <v-card-text>
       <v-chip-group v-model="stallSelection" style="margin-left: 17%;" column active-class="green white--text">
-        <v-chip v-for="stall in stallData" :key="stall.id" :disabled="stall.occupied">{{stall.occupied ? 'occupied for:'+ stall.duration: stall.id}}</v-chip>
+        <v-chip v-for="stall in storeFloorStalls" :key="stall.id" :disabled="stall.occupied">{{stall.occupied ? 'occupied for:'+ stall.duration: stall.id}}</v-chip>
       </v-chip-group>
     </v-card-text>
     <v-card-title text="center">Reserve Ahead</v-card-title>
@@ -25,8 +25,8 @@
       </v-chip-group>
       <v-divider class="mx-2"></v-divider>
       <v-row>
-                      <v-btn style="margin-top: 35px; width: 45%;"
-          elevation="2" @click="bookStall(stallSelection, genderSelection, floor_num)"
+        <v-btn style="margin-top: 35px; width: 45%;"
+          elevation="2" @click="bookStall(occupiedID)"
         >Book Now</v-btn>
                 <v-btn style=" margin-top: 35px; width: 45%; margin-left: 10%;" 
           elevation="2"
@@ -37,12 +37,12 @@
 </template>
 
 <script>
+import {  mapActions, mapState } from "vuex";
 export default {
-    name: 'floor-card',
+    name: 'floor-card-store',
     data: ()=> ({
         selection: 1,
         stallSelection: null,
-        genderSelection: 0,
         isOrange: true,
         timerOne: null,
         timerOneRunning: false,
@@ -52,7 +52,6 @@ export default {
         timeTwo: 0,
         occupiedCheckOne: false,
         occupiedCheckTwo: false,
-        colorOfCard: 'orange',
     }),
     created() {
       this.timerOneRunning == false;
@@ -97,49 +96,69 @@ export default {
         },
         stallData: {
           type: Array,
+        },
+        //storeFloorStalls is the variable passed from the state vuex
+        storeFloorStalls: {
+            type: Array
         }
     },
-    watch: {
-      genderSelection: function() {
-        if(this.genderSelection == 0){
-          this.colorOfCard = 'orange';
-          this.$emit('clicked', 'b');
-        }
-        else if (this.genderSelection == 1){
-          this.colorOfCard = 'teal';
-          this.$emit('clicked', 'f')
-        }
+    computed: {
+      ...mapState(["stalls", "currentGender"]),
+      genderSelection: {
+        get () {
+          var stateGender = this.$store.state.currentGender;
+          if(stateGender == 'b'){
+            return 0;
+          }
+          else if(stateGender =='f'){
+            return 1;
+          }
+          else {
+            return 1;
+          }
+        },
+        set (value) {
+          if (value == 0){
+            this.updateGender('b');
+          }
+          else if(value == 1){
+            this.updateGender('f');
+          }
+          else {
+             this.updateGender('f');
+          }
+        },
       },
-      stallSelection: function() {
-        console.log(this.stallSelection);
-        this.checkForDisabled();
+    occupiedID (value) {
+        console.log(value);
+        return this.storeFloorStalls[this.stallSelection].id;
+        },
+      colorOfCard: function() {
+        if(this.genderSelection == 0){
+          return 'orange'
+        }
+        else if(this.genderSelection == 1){
+          return 'teal'
+        }
+        else {
+          return 'red';
+        }
       }
     },
     methods: {
-      bookStall(stallNum, gender, floor){
-        if(stallNum!=null && gender!=null && floor!=null){
-          console.log('stall has been booked');
-          var id = '';
-          //create the id to compare w. firebase data
-          if(gender == 0){ //gender=0 is a male
-            id = String([floor+1, 'b', stallNum+1])
-            id = id.replace(/[, ]+/g, "").trim();
+      ...mapActions(["updateGender"]),
+      bookStall(value){
+          if(value!=null){
+              this.$emit('booking', value);
           }
-          if(gender ==1){ //gender=1 is a female
-            id = String([floor+1, 'f', stallNum+1])
-            id = id.replace(/[, ]+/g, "").trim();
+          else {
+            alert('Error: some data was not added, therefore booking not available at this time.') 
           }
-          this.$emit('booking', id)
-        }
-        else {
-          alert('Error: some data was not added, therefore booking not available at this time.')
-        }
       },
       checkForDisabled() {
         for(var stall in this.stallData) {
           if(this.stallData[stall].occupied){
               console.log('Stall '+ this.stallData[stall].id + ' has been occupied for: '+ this.stallData[stall].duration);
-
           }
           else if(!this.stallData[stall].occupied){
               console.log('Stall '+ this.stallData[stall].id + ' has not been occupied for')
